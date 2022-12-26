dc764e5720a1cdb6511c19d49fc1864edefd4a26
AlexSkolnik <alexfbr3@gmail.com>
2022-12-26 11:34:48 +0300

    Добавил Swagger

---
+++ src/Infrastructure/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Infrastructure.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        return services
+            .AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Dodo.Loyalty.WebApi"
+                });
+
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+    }
+
+    public static IApplicationBuilder UseSwaggerWithUi(this IApplicationBuilder app)
+    {
+        return app
+            .UseSwagger()
+            .UseSwaggerUI();
+    }
+}
